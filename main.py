e1ba40f7c398795c04132d5d58fbcd8c8af9ddf0
aheldes <59527963+aheldes@users.noreply.github.com>
2023-02-26 12:23:23 +0100

    AD-9

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from djitellopy import Tello
+import time, cv2
+from threading import Thread
+
+from djitellopy import Tello, BackgroundFrameRead
 from djitellopy import TelloException
 
 
@@ -7,14 +10,42 @@ def main() -> None:
 
     try:
         tello.connect()
+        tello.streamon()
+        frame_read = tello.get_frame_read()
+        
+        video = VideoRecorder(frame_read)
+        recorder = Thread(target=video.start)
+        recorder.start()
+        
         tello.takeoff()
         for _ in range(4):
             tello.move_forward(20)
             tello.rotate_clockwise(90)
+            cv2.imwrite("picture.png", frame_read.frame)
     except TelloException as e:
         print(e)
     finally:
         tello.land()
+        
+        video.keepRecording = False
+        recorder.join()
+
+class VideoRecorder:
+    
+    def __init__(self, frame_read: BackgroundFrameRead) -> None:
+        self.frame_read = frame_read
+        self.keepRecording = True
+        self.video = None
+        
+    def start(self) -> None:
+        height, width, _ = self.frame_read.frame.shape
+        video = cv2.VideoWriter('video.avi', cv2.VideoWriter_fourcc(*'XVID'), 30, (width, height))
+
+        while self.keepRecording:
+            video.write(self.frame_read.frame)
+            time.sleep(1 / 30)
+
+        video.release()
 
 
 if __name__ == "__main__":
