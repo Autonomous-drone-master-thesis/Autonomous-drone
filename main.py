f00e9ca790606b1f2120ec81b32e10a6307fff9d
aheldes <59527963+aheldes@users.noreply.github.com>
2023-02-27 18:17:32 +0100

    Format changes

--- main.py
+++ main.py
@@ -1,35 +1,36 @@
 import time
 import cv2
-import logging
-from threading import Thread
+import numpy as np
 from djitellopy import Tello
 
-logger = logging.getLogger('main')
-logger.setLevel(logging.INFO)
-handler = logging.StreamHandler()
-formatter = logging.Formatter('[%(levelname)s] %(filename)s - %(lineno)d - %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+
+class Drone:
+    def __init__(self):
+        self.tello = Tello()
+
+    def run(self):
+        self.tello.connect()
+
+        self.tello.streamoff()
+        self.tello.stream_on()
+
+        frame_read = self.tello.get_frame_read()
+
+        while i < 1000:
+            frame = cv2.cvtColor(frame_read.frame, cv2.COLOR_RGB2BGR)
+            frame = np.rot90(frame)
+            frame = np.flipud(frame)
+
+            cv2.imshow("Tello", frame)
+
+            i += 1
+
+        self.tello.end()
+
 
 def main():
-    tello = Tello()
-    tello.connect()
-
-    try:
-        tello.streamon()
-        frame_read = tello.get_frame_read()
-
-        time.sleep(3)
-
-        while True:
-            img = cv2.cvtColor(frame_read.frame, cv2.COLOR_RGB2BGR)
-            cv2.imshow("Streaming", img)
-
-    except Exception as e:
-        logger.error('Error: %s', e)
-
-    finally:
-        tello.streamoff()
+    drone = Drone()
+    drone.run()
 
 
 if __name__ == "__main__":
