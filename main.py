49fe948cf022a68f8e09f7e3d46412adbf4252f7
aheldes <59527963+aheldes@users.noreply.github.com>
2023-02-27 18:37:18 +0100

    AD-9 Final

--- main.py
+++ main.py
@@ -12,20 +12,17 @@ class Drone:
         self.tello.connect()
 
         self.tello.streamoff()
-        self.tello.stream_on()
+        time.sleep(2)
+        self.tello.streamon()
+        time.sleep(5)
 
-        frame_read = self.tello.get_frame_read()
-
-        while i < 1000:
-            frame = cv2.cvtColor(frame_read.frame, cv2.COLOR_RGB2BGR)
-            frame = np.rot90(frame)
-            frame = np.flipud(frame)
+        while True:
+            frame = cv2.cvtColor(self.tello.get_frame_read().frame, cv2.COLOR_RGB2BGR)
+            frame = cv2.resize(frame, (360, 240))
 
             cv2.imshow("Tello", frame)
 
-            i += 1
-
-        self.tello.end()
+            cv2.waitKey(1)
 
 
 def main():
