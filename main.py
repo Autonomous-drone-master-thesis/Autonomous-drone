2697ef2061756de56b6e95519d6067957a861719
aheldes <adrian.heldes@telekom.com>
2023-03-06 15:07:23 +0100

    Add docstring

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from kivy.app import App
 from kivy.uix.label import Label
 
-class HelloWorldApp(App):
-    def build(self):
-        return Label(text='Hello, World!')
 
-if __name__ == '__main__':
+class HelloWorldApp(App):
+    """
+    A Kivy application that displays "Hello, World!" in a Label widget.
+    """
+    def build(self) -> Label:
+        return Label(text="Hello, World!")
+
+
+if __name__ == "__main__":
     HelloWorldApp().run()
