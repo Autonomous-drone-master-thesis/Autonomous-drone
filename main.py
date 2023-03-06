70874ccc34b0b4593c235f2c232a3983d2e022ea
aheldes <adrian.heldes@telekom.com>
2023-03-06 15:07:13 +0100

    Add basic hello world Kivi

--- main.py
+++ main.py
@@ -1,34 +1,9 @@
-import time
-import cv2
-import numpy as np
-from djitellopy import Tello
+from kivy.app import App
+from kivy.uix.label import Label
 
+class HelloWorldApp(App):
+    def build(self):
+        return Label(text='Hello, World!')
 
-class Drone:
-    def __init__(self):
-        self.tello = Tello()
-
-    def run(self):
-        self.tello.connect()
-
-        self.tello.streamoff()
-        time.sleep(2)
-        self.tello.streamon()
-        time.sleep(5)
-
-        while True:
-            frame = cv2.cvtColor(self.tello.get_frame_read().frame, cv2.COLOR_RGB2BGR)
-            frame = cv2.resize(frame, (360, 240))
-
-            cv2.imshow("Tello", frame)
-
-            cv2.waitKey(1)
-
-
-def main():
-    drone = Drone()
-    drone.run()
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    HelloWorldApp().run()
