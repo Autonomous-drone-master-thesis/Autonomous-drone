0f69baa6df5095ac1fae3903a4541563d96c9882
aheldes <59527963+aheldes@users.noreply.github.com>
2023-04-26 08:05:13 +0200

    Add video capture

--- test_scripts/drone_stream.py
+++ test_scripts/drone_stream.py
@@ -1,33 +1,55 @@
 import time
 import cv2
+from threading import Thread
 from djitellopy import Tello
 
 
 class Drone:
     def __init__(self):
         self.tello = Tello()
+        self.recording = False
+        self.video = None
+        self.recorder_thread = None
+        self.tello.connect()
+        self.tello.streamon()
 
     def run(self):
-        self.tello.connect()
-
-        self.tello.streamoff()
-        time.sleep(2)
-        self.tello.streamon()
-        time.sleep(5)
-
         while True:
-            frame = cv2.cvtColor(self.tello.get_frame_read().frame, cv2.COLOR_RGB2BGR)
-            frame = cv2.resize(frame, (360, 240))
+            frame = self.tello.get_frame_read().frame
 
             cv2.imshow("Tello", frame)
 
-            cv2.waitKey(1)
+            key = cv2.waitKey(1)
+            if key == ord('q'):
+                self.stop_recording()
+                self.tello.streamoff()
+                break
+    
+    def start_recording(self):
+        print('starting recording')
+        height, width, _ = self.tello.get_frame_read().frame.shape
+        self.video = cv2.VideoWriter('test_video.avi', cv2.VideoWriter_fourcc(*"XVID"), 30, (width, height))
+        self.recording = True
+        print('starting threads')
+        self.recorder_thread = Thread(target=self._keep_recording)
+        self.recorder_thread.start()
+        print('Threads started')
+    
+    def _keep_recording(self) -> None:
+        while self.recording:
+            self.video.write(self.tello.get_frame_read().frame)
+            time.sleep(1 / 30)
+        self.video.release()
+    
+    def stop_recording(self) -> None:
+        self.recording = False
+        self.recorder_thread.join()
 
 
 def main():
     drone = Drone()
+    drone.start_recording()
     drone.run()
-
 
 if __name__ == "__main__":
     main()
